0de21e49efe89086a34174ef0dd1100f58e1f6d4
Tomusdrw <tomusdrw@gmail.com>
2015-05-08 11:13:36 +0200

    Fixing navigation.

--- tasks.js
+++ tasks.js
@@ -1,5 +1,10 @@
 (function() {
   var currentTask = 0;
+  var currentRanking = {
+    0: true
+  };
+  var sendDone = function() {};
+
 
   injectStyles();
   // Add embed class
@@ -7,48 +12,143 @@
     createEmbedMode();
   }
 
-
-
   // Functions
+  function changeTask(no) {
+    var newTask = no;
+    var max = $$('section').length;
+    if (newTask >= max) {
+      newTask = max - 1;
+    }
 
-  function changeTask($btnNext, $btnPrev, mod) {
-    var newTask = currentTask + mod;
+    function swapActive($els) {
+      $els[currentTask].classList.remove('active');
+      $els[newTask].classList.add('active');
+    }
 
-    var $sections = $$('section');
-    $sections[currentTask].classList.remove('active');
-    $sections[newTask].classList.add('active');
-
-    var method = (newTask >= $sections.length - 1) ? 'add' : 'remove';
-    $btnNext.classList[method]('hidden');
-    method = (newTask <= 0) ? 'add' : 'remove';
-    $btnPrev.classList[method]('hidden');
+    swapActive($$('section'));
+    swapActive($$('.tasks-nav-item'));
 
     currentTask = newTask;
+
+    // Fix Done button
+    var navDone = $('.nav-done');
+    if (navDone) {
+      if (currentRanking[newTask]) {
+        navDone.innerHTML = 'Not Done';
+        navDone.classList.add('nav-done-muted');
+      } else {
+        navDone.innerHTML = 'Done';
+        navDone.classList.remove('nav-done-muted');
+      }
+    }
   }
+
 
   function createEmbedMode() {
     $('html').classList.add('embed');
     // Add navigation
     createEmbedNavigation();
+    createEmbedUtils();
+    createRankingListeners();
+    // activate first section
+    changeTask(0);
+  }
+
+  function createRankingListeners() {
+    var parentIdx = window.location.search.indexOf('url=');
+    var url = parentIdx !== -1 ? window.location.search.substr(parentIdx + 4) : window.location.toString();
+
+    window.addEventListener('message', function(msg) {
+
+      try {
+        var rank = JSON.parse(msg.data);
+        if (!rank || !rank.isRanking) {
+          return;
+        }
+        currentRanking = rank;
+      } catch (e) {
+        console.warn(e, msg);
+      }
+      currentRanking = currentRanking || {};
+      createEmbedNavigation();
+    });
+
+    sendDone = function(currentTask, isDone) {
+      var msg = {
+        currentTask: currentTask,
+        isDone: isDone
+      };
+      currentRanking[currentTask] = isDone;
+      createEmbedNavigation();
+      window.parent.postMessage(JSON.stringify(msg), url);
+    };
+  }
+
+  function getTitle($s) {
+    var check = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', '.title'];
+    return check.reduce(function(found, sel) {
+      if (found) {
+        return found;
+      }
+      var h = $s.querySelector(sel);
+      if (h) {
+        return h.textContent;
+      }
+    }, false) || '';
+  }
+
+  function removePreviousNav() {
+    var $x = $('nav.tasks-nav');
+    if ($x) {
+      $x.parentNode.removeChild($x);
+    }
   }
 
   function createEmbedNavigation() {
-    var $btnNext = $el('button.btn.btn-default.btn-xs.nav-next');
-    var $btnPrev = $el('button.btn.btn-link.btn-xs.nav-prev');
+    removePreviousNav();
+    var $sections = $$('section');
+    var $nav = $el('nav.tasks-nav');
+    $('.container').appendChild($nav);
+
+    [].map.call($sections, function($s, idx) {
+
+      var $li = $el('li.tasks-nav-item');
+      if (currentRanking[idx]) {
+        $li.classList.add('done');
+      }
+      var $item = $el('a');
+      $li.appendChild($item);
+      $item.addEventListener('click', changeTask.bind(null, idx));
+      $item.title = getTitle($s);
+      $item.innerHTML = '&lowast;';
+      return $li;
+    }).map(function($el) {
+      $nav.appendChild($el);
+    });
+
+    // Make sure that is marked active
+    changeTask(currentTask);
+  }
+
+  function createEmbedUtils() {
+    var $btnDone = $el('button.btn.btn-primary.btn-sm.nav-done');
+    var $btnNewWindow = $el('a.btn.btn-link.btn-xs.nav-new-window');
     var $firstChild = $('.container > *:first-child');
     var $container = $('.container');
-    $container.insertBefore($btnPrev, $firstChild);
-    $container.insertBefore($btnNext, $firstChild);
+    $container.insertBefore($btnNewWindow, $firstChild);
+    $container.insertBefore($btnDone, $firstChild);
 
-    $btnNext.innerHTML = 'Next Task &rarr;';
-    $btnPrev.innerHTML = '&larr;';
+    $btnDone.innerHTML = 'Done';
 
-    $btnNext.addEventListener('click', changeTask.bind(null, $btnNext, $btnPrev, +1));
-    $btnPrev.addEventListener('click', changeTask.bind(null, $btnNext, $btnPrev, -1));
+    $btnDone.addEventListener('click', function() {
+      sendDone(currentTask, !currentRanking[currentTask]);
+      changeTask(currentTask + 1);
+    });
 
-    // activate first section
-    changeTask($btnNext, $btnPrev, 0);
-
+    $btnNewWindow.innerHTML = '&there4;';
+    $btnNewWindow.title = 'Open In Separate Window';
+    $btnNewWindow.target = '_blank';
+    $btnNewWindow.href = window.location.toString().replace(/\?.*/, '');
   }
 
   function injectStyles() {
@@ -71,6 +171,7 @@
   function $text(text) {
     return document.createTextNode(text);
   }
+
   function $el(name) {
     var parts = name.split('.');
     var tagName = parts[0];
